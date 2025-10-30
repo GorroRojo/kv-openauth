0d082b08cd159e4b426e43e856f0357b5c1e5a71
Gorro_Rojo <gorro.rojo@gmail.com>
2025-10-30 20:19:33 -0300

    authorize same domain and other workers

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import { PasswordUI } from "@openauthjs/openauth/ui/password";
 import { createSubjects } from "@openauthjs/openauth/subject";
 import { object, string } from "valibot";
 import { Resend } from "resend";
-import { CodeProvider } from "@openauthjs/openauth/provider/code";
-import { CodeUI } from "@openauthjs/openauth/ui/code";
 
 // This value should be shared between the OpenAuth server Worker and other
 // client Workers that you connect to it, so the types and schema validation are
@@ -38,9 +36,21 @@ export default {
         params: Object.fromEntries(url.searchParams.entries()),
       });
     }
-
     // The real OpenAuth server code starts here:
     return issuer({
+      allow: async (input, req) => {
+        let noSubdomain = (hostname: string) =>
+          hostname.split(".").slice(1).join(".");
+        let redirect_hostname = new URL(
+          url.searchParams.get("redirect_uri") ?? ""
+        ).hostname;
+        let redirect_hostname_nosub = noSubdomain(redirect_hostname);
+        return (
+          redirect_hostname == "localhost" ||
+          redirect_hostname_nosub == noSubdomain(url.hostname) ||
+          redirect_hostname_nosub == "gorro-rojo.workers.dev"
+        );
+      },
       storage: CloudflareStorage({
         namespace: env.AUTH_STORAGE,
       }),
@@ -65,15 +75,16 @@ export default {
               change_prompt: "¿Olvidaste tu contraseña?",
               code_resend: "Volver a mandar código",
               code_return: "Volver a",
-              error_email_taken: "Ya existe una cuenta con esta dirección de correo",
+              error_email_taken:
+                "Ya existe una cuenta con esta dirección de correo",
               error_invalid_code: "El código es incorrecto.",
               error_invalid_email: "La dirección de correo no es válida.",
               error_invalid_password: "La contraseña no es válida.",
               error_password_mismatch: "Las contraseñas no coinciden.",
               error_validation_error: "La contraseña no cumple los requisitos.",
               input_code: "Código",
-              input_email:"Dirección de correo",
-              input_password:"Contraseña",
+              input_email: "Dirección de correo",
+              input_password: "Contraseña",
               input_repeat: "Repetir contraseña",
               login: "Ingresar",
               login_description: "Ingresar con tu correo",
@@ -84,7 +95,6 @@ export default {
               register_prompt: "¿No tenés cuenta?",
               register_title: "Bienvenide al sitio", //???
               // validatePassword: (psw) => psw.length < 8 ? "La contraseña debe tener al menos 8 caracteres" : undefined
-
             },
           })
         ),
@@ -95,7 +105,7 @@ export default {
         favicon: "https://kinkyvibe.ar/favicon-32x32.png",
         logo: "https://kinkyvibe.ar/android-chrome-512x512.png",
         radius: "lg",
-        background:"#eee"
+        background: "#eee",
       },
       success: async (ctx, value) => {
         return ctx.subject("user", {
